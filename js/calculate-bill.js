5202ae7f0cb1691ea5f3681b243b4656a0ffa928
bootcamp <myezasifiso11@gmail.com>
2018-04-16 10:22:48 +0200

    done with the first widget

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,10 +1,37 @@
 //get a reference to the calculate button
-
+var calcBillBtn = document.querySelector(".calculateBtn");
 //get a reference to the billTotal element
-
+var billTotal =  document.querySelector(".billTotal");
 //get a reference to the billString
+var billStringElement = document.querySelector(".billString");
+
+
+
+
+
 
 //create the function that will be called when the calculate button is pressed
+// add event listener
+function calculateBtnClicked(){
+    var sum = 0;
+    var inputString = billStringElement.value;
+     var list = inputString.split(',');
+
+     list.forEach(function(item){
+       if(item.trim() === "call"){
+         sum += 2.75;
+       }
+       else if(item.trim() === "sms"){
+         sum += 0.75;
+       }
+     })
+    //  console.log(sum);
+    var totalBill = sum.toFixed(2);
+    billTotal.innerHTML = totalBill;
+
+}
+
+calcBillBtn.addEventListener('click', calculateBtnClicked);
 //  * this function should read the string value entered - split it on a comma.
 //  * loop over all the entries in the the resulting list
 //  * check if it is a call or an sms and add the right amount to the overall total
